4c44a2de9b93b906c3fddfafb146c9feaf22e865
kk <+>
2023-03-13 17:51:20 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
 // Write your code here
+const num1 =2 ;
+const num2 = 31;
+const multiply = num1 * num2;
+const random = Math.floor(Math.random() * 10) + 1;
+const num3 =8;
+const num4 = 2;
+const mod = num3 / num4;
+const max = Math.max(16,17,20);
